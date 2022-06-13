979bfce23750d3967f0b10b926272e5795424fad
Symbiote-ux <tnikhil422@gmail.com>
2022-06-13 15:05:58 +0530

    refactoring

--- src/waterBoard.js
+++ src/waterBoard.js
@@ -2,34 +2,53 @@ class WaterBoard {
   constructor(commands) {
     this.commands = commands;
     this.apartmentDetails = { guest: 0 };
+    this.borewellRate = 1.5;
+    this.personCapacity = 300;
+    this.slabRate = [3000, 8, 1500, 5, 500, 3];
+  }
+  estimateCost(volume) {
+    let cost = 0;
+    for (let i = 0; i < this.slabRate.length; i += 2) {
+      if (volume > this.slabRate[i]) {
+        volume -= this.slabRate[i];
+        cost += volume * this.slabRate[i + 1];
+        volume = thi.slabRate[i];
+      }
+    }
+    return {cost,}
   }
   calcGuestBill() {
     const guestCount = this.apartmentDetails['guest'];
-    const capacity = 300;
-    const quantity = guestCount * capacity;
-    let value = 0;
-    let volume = quantity;
-    const rateList = [3000, 8, 1500, 5, 500, 3];
-    for (let i = 0; i < rateList.length; i += 2) {
-      if (volume > rateList[i]) {
-        volume = volume - rateList[i];
-        value += volume * rateList[i + 1];
-        volume = rateList[i];
-      }
-    }
-    const rate = 2;
-    const remCost = volume * rate;
-    const cost = value + remCost;
+    const quantity = guestCount * this.personCapacity;
+
+    // const guestCount = this.apartmentDetails['guest'];
+    // const capacity = 300;
+    // const quantity = guestCount * capacity;
+    // let value = 0;
+    // let volume = quantity;
+    // const rateList = [3000, 8, 1500, 5, 500, 3];
+    // for (let i = 0; i < rateList.length; i += 2) {
+    //   if (volume > rateList[i]) {
+    //     volume = volume - rateList[i];
+    //     value += volume * rateList[i + 1];
+    //     volume = rateList[i];
+    //   }
+    // }
+    // const rate = 2;
+    const cost = value + volume * rate;
     return { quantity, cost };
   }
+  getVolume(total) {
+    const corpRatio = Number(this.apartmentDetails['corp']);
+    const borRatio = Number(this.apartmentDetails['boreWell']);
+    return Math.floor((total * corpRatio) / (corpRatio + borRatio));
+  }
   calcBill(total) {
+    const corpVol = this.getVolume(total);
+    const borVol = total - corpVol;
     const { quantity, cost } = this.calcGuestBill();
-    const crpRatio = Number(this.apartmentDetails['corporation']);
-    const borRatio = Number(this.apartmentDetails['borewell']);
-    const crpVol = Math.floor((total * crpRatio) / (crpRatio + borRatio));
-    const borVol = total - crpVol;
-    const totalVol = crpVol + borVol + quantity;
-    const totalCost = crpVol + Math.ceil(borVol * 1.5) + cost;
+    const totalVol = corpVol + borVol + quantity;
+    const totalCost = corpVol + Math.ceil(borVol * this.borewellRate) + cost;
     return { totalVol, totalCost };
   }
   fetchBill() {
@@ -41,20 +60,25 @@ class WaterBoard {
     return this.calcBill(capacity.type3);
   }
   addGuests(count) {
-    let guestPresent = this.apartmentDetails.guest;
+    const guestPresent = this.apartmentDetails.guest;
     this.apartmentDetails['guest'] = Number(guestPresent) + Number(count);
   }
   allotWater(type, ratio) {
     this.apartmentDetails['type'] = type;
-    const [corporation, borewell] = ratio.split(':');
-    this.apartmentDetails['corporation'] = corporation;
+    const [corp, borewell] = ratio.split(':');
+    this.apartmentDetails['corp'] = corp;
     this.apartmentDetails['borewell'] = borewell;
+  }
+  parse(command) {
+    if (command[0] == 'ALLOT_WATER') {
+      return this.allotWater(command[1], command[2]);
+    }
+    return this.addGuests(command[1]);
   }
   getBill() {
     this.commands.pop();
-    this.commands.forEach((cmd) => {
-      if (cmd[0] == 'ALLOT_WATER') this.allotWater(cmd[1], cmd[2]);
-      if (cmd[0] == 'ADD_GUESTS') this.addGuests(cmd[1]);
+    this.commands.forEach((command) => {
+      this.parse(command);
     });
     return this.fetchBill();
   }
